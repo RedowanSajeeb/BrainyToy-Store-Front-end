b18d6e71d709e14a503695bf98691a79ee08bf4f
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-18 18:47:42 +0600

    navbar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+import Navbar from "../Pages/Sheard/Navbar";
+
+const Main = () => {
+    return (
+        <div className="">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
