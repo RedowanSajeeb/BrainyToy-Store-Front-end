84d72b6ded50d755a8af0c1797498352a4883252
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-18 17:50:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+import { Button } from "@material-tailwind/react";
+
+const App = () => {
+  return (
+    <div>
+      <img
+        className="h-24"
+        src="https://s3-us-west-2.amazonaws.com/issuewireassets/primg/105657/skillotoyscom-a-curated-educational-toys-learning-furniture-online-store1345431857.png"
+        alt=""
+      />
+      <Button>Button</Button>;
+    </div>
+  );
+};
+
+export default App;
