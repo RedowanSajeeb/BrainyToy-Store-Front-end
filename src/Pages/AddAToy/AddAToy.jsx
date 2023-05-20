ecd9bc79fe3cecf29e08cb460f5de76277176476
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-20 19:47:53 +0600

    todo-find

--- src/Pages/AddAToy/AddAToy.jsx
+++ src/Pages/AddAToy/AddAToy.jsx
@@ -1,59 +1,100 @@
 // import React from 'react';
-import { Input, Option, Select, Textarea } from "@material-tailwind/react";
+import { Button, Input, Option, Select, Textarea } from "@material-tailwind/react";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 const AddAToy = () => {
   const { user } = useContext(AuthContext);
   console.log(user?.displayName);
+
+
+  const addANewToyHandlersCallback = (event) => {
+     event.preventDefault();
+     
+     const fieldValue = event.target;
+     const toyPictureURL = fieldValue.toyPictureURL.value;
+     const toyName = fieldValue.toyName.value;
+     const email = fieldValue.email.value;
+     const sellername = fieldValue.sellername.value;
+     const price = fieldValue.price.value;
+     const rating = fieldValue.rating.value;
+     const quantity = fieldValue.quantity.value;
+     const category = fieldValue.category.value;
+    const description = fieldValue.description.value;
+ console.log(toyPictureURL, toyName, email,sellername, price, quantity, category, price, rating, quantity,description);
+
+
+
+     
+  }
+
+
   return (
     <div>
-      <h1 className="text-center md:mt-10 mt-5 md:text-5xl">
-        Add A New Toy in the{" "}
+      <h1 className="text-center  mt-5 md:text-5xl">
+        Add A New Toy in the
         <span className="text-orange-500">BrainYToy Store</span>{" "}
       </h1>
       <p className="text-center mt-3 text-gray-500">
         &quot;Wonderland of Knowledge: Unleashing Potential with Educational
         Toys! &quot;
       </p>
-      <div
-        className="max-w-5xl mx-auto md:mt-16 mt-5 
-        border md:p-10 gap-5 w-full h-full mb-20 p-4 rounded-lg md:mb-24"
+      <form
+        onSubmit={addANewToyHandlersCallback}
+        className="max-w-5xl mx-auto  mt-5 
+        border md:p-7 gap-5 w-full h-full mb-20 p-4 rounded-lg md:mb-24"
       >
         <div
           className="max-w-5xl mx-auto  mt-5 
        grid md:grid-cols-2 border  gap-5 w-full h-full p-4 rounded-lg mb-5"
         >
           <div className="flex  flex-col gap-6">
-            <Input color="blue" label="Picture URL of the toy" />
-            <Input color="purple" label="Toy Name" />
             <Input
+              name="toyPictureURL"
+              color="blue"
+              label="Picture URL of the toy"
+            />
+            <Input name="toyName" color="purple" label="Toy Name" />
+            <Input
+              name="sellername"
               color="indigo"
               label="Seller name"
               defaultValue={user?.displayName}
             />
             <Input
+              name="email"
               color="teal"
               label="Seller email"
               defaultValue={user?.email}
             />
           </div>
           <div className="flex  flex-col gap-6">
-            <Input color="blue" type="number" label="Price" />
-            <Input color="purple" label="Rating" />
-            <Input color="indigo" type="number" label="Available quantity" />
-            <Select label="Category ">
-              <Option>Math Toys</Option>
-              <Option>Science Toys</Option>
-              <Option>Language Toys</Option>
-              <Option>engineering tools</Option>
+            <Input name="price" color="blue" type="number" label="Price" />
+            <Input name="rating" color="purple" label="Rating" />
+            <Input
+              name="quantity"
+              color="indigo"
+              type="number"
+              label="Available quantity"
+            />
+            <Select name="category" label="Category" value={null}>
+              <Option value="Math Toys">Math Toys</Option>
+              <Option value="Science Toys">Science Toys</Option>
+              <Option value="Language Toys">Language Toys</Option>
+              <Option value="engineering tools">engineering tools</Option>
               {/* <Option>Material Tailwind Svelte</Option> */}
             </Select>
           </div>
         </div>
-        <div className="w-full mx-auto ">
-          <Textarea label="Detail description" />
+        <div className="w-full    mx-auto ">
+          <Textarea name="description" label="Detail description" />
         </div>
-      </div>
+
+        <button className="w-full">
+          <Button className="w-full mt-4 " variant="outlined">
+            Add A Toy
+          </Button>
+        </button>
+      </form>
     </div>
   );
 };
