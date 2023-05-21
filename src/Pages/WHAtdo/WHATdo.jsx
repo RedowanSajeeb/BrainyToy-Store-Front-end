3a553f93a7afdf0fbd5b4a7effe149d524a56b84
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-21 23:48:34 +0600

    done

--- src/Pages/WHAtdo/WHATdo.jsx
+++ src/Pages/WHAtdo/WHATdo.jsx
@@ -2,8 +2,11 @@
 
 const WHATdo = () => {
     return (
-      <div className="md:flex justify-center items-center ms-4 me-4 mt-5 md:mt-14">
-        <div>
+      <div
+        className="md:flex justify-center items-center ms-4 me-4 mt-5 md:mt-14"
+        data-aos="fade-right"
+      >
+        <div data-aos="fade-up-right">
           <img
             src="https://junotoys.themerex.net/wp-content/uploads/2021/09/creative-image-1-copyright.png"
             alt=""
@@ -11,10 +14,12 @@ const WHATdo = () => {
         </div>
         <div>
           <h4 className="text-2xl font-bold">WHAT WE DO</h4>
-          <h1 className=" mt-2 text-6xl text-orange-400">We help learn for kids</h1>
+          <h1 className=" mt-2 text-6xl text-orange-400">
+            We help learn for kids
+          </h1>
           <p className="mt-3 text-3xl">
-            Fun, educational activities for every <br /> occasion with premium toys and
-            snacks. <br /> Learn more about our services!
+            Fun, educational activities for every <br /> occasion with premium
+            toys and snacks. <br /> Learn more about our services!
           </p>
         </div>
       </div>
